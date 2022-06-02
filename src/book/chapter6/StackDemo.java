b52b62214602dce3e88c54b9785e1fbfbf874fbe
Frost2007 <vip.psn2007@mail.ru>
2022-06-02 16:56:10 +0400

    charArrayExample

---
+++ src/book/chapter6/StackDemo.java
@@ -0,0 +1,18 @@
+package book.chapter6;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+        stack.push(10);
+    }
+}
