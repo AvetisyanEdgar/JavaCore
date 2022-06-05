985843bfd2ced1f8d3e851e5ffcf3fdb88646040
Frost2007 <vip.psn2007@mail.ru>
2022-06-05 14:59:44 +0400

    charArrayExample

--- src/classwork/lesson10/BoxDemo.java
+++ src/classwork/lesson10/BoxDemo.java
@@ -2,10 +2,10 @@ package classwork.lesson10;
 
 public class BoxDemo {
     public static void main(String[] args) {
-        Box myBox = new Box();
-        System.out.println(myBox.depth);
-        myBox.width = 5;
-        myBox.depth = 30;
-        myBox.height = 45;
+        Box myBox = new Box(4, 5, 6);
+        Box myBox2 = new Box(4, 5, 6);
+        
+        System.out.println(myBox.equalTo(myBox2));
+
     }
 }
